31a86ba2b9ebca2bf210b3724e90e2dc2052bb25
Habillah Darma <hdhmmz@gmail.com>
2026-05-04 11:01:20 +0700

    fix: config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
+
 
 const nextConfig = {
-  output: isProd ? "export" : undefined,
-  basePath: isProd ? "/Website_Latihan_LMS" : "",
   images: {
     unoptimized: true,
   },
